073dae5d3f0d4924ab024412dce3f641201ca5d6
Carlos Rodrigues <cerf@furg.br>
2024-07-12 20:19:05 -0300

    feat: Add OrderId value object for domain entities

---
+++ common/src/domain/value_object/order_id.rs
@@ -0,0 +1,34 @@
+use super::base_id::BaseId;
+
+pub struct OrderId<T>
+where
+    T: From<uuid::Uuid> + Into<uuid::Uuid>,
+{
+    id: T,
+}
+
+impl<T> BaseId<T> for OrderId<T>
+where
+    T: From<uuid::Uuid> + Into<uuid::Uuid>,
+{
+    fn new(id: T) -> Self {
+        Self { id }
+    }
+
+    fn get_id(&self) -> &T {
+        &self.id
+    }
+}
+
+#[cfg(test)]
+mod order_id_tests {
+    use super::*;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_order_id() {
+        let id = Uuid::now_v7();
+        let order_id = OrderId::new(id);
+        assert_eq!(order_id.get_id(), &id);
+    }
+}
