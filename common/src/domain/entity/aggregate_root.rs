d98ad967ca481b1067824d23f70228a9fca5fc46
Carlos Rodrigues <cerf@furg.br>
2024-07-12 17:23:14 -0300

    feat: Add AggregateRoot trait for domain entities
    This commit adds the `AggregateRoot` trait to the `common` module in order to provide a common interface for aggregate root entities. The trait extends the `BaseEntity` trait and does not include any additional methods. The implementation has been commented out for now.

---
+++ common/src/domain/entity/aggregate_root.rs
@@ -0,0 +1,34 @@
+use super::BaseEntity;
+
+pub trait AggregateRoot<ID: PartialEq>: BaseEntity<ID> {}
+
+#[cfg(test)]
+mod aggregate_root_tests {
+    use crate::domain::entity::{AggregateRoot, BaseEntity};
+
+    #[test]
+    fn test_aggregate_root() {
+        struct ConcreteAggregateRoot<ID> {
+            id: ID,
+        }
+        impl<ID: PartialEq> BaseEntity<ID> for ConcreteAggregateRoot<ID> {
+            fn get_id(&self) -> &ID {
+                &self.id
+            }
+
+            fn new(id: ID) -> Self {
+                Self { id }
+            }
+
+            fn set_id(&mut self, id: ID) {
+                self.id = id;
+            }
+        }
+
+        impl<ID: PartialEq> AggregateRoot<ID> for ConcreteAggregateRoot<ID> {}
+
+        let id = 1;
+        let aggregate_root = ConcreteAggregateRoot::new(id);
+        assert_eq!(aggregate_root.get_id(), &id);
+    }
+}
