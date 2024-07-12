ae81f6c0397fec0b193dd6c0e7ddf21d190bce1f
Carlos Rodrigues <cerf@furg.br>
2024-07-12 20:27:48 -0300

    feat: Add RestaurantId value object for domain entities

---
+++ common/src/domain/value_object/restaurant_id.rs
@@ -0,0 +1,34 @@
+use super::base_id::BaseId;
+
+pub struct RestaurantId<T>
+where
+    T: From<uuid::Uuid> + Into<uuid::Uuid>,
+{
+    id: T,
+}
+
+impl<T> BaseId<T> for RestaurantId<T>
+where
+    T: From<uuid::Uuid> + Into<uuid::Uuid>,
+{
+    fn new(id: T) -> Self {
+        Self { id }
+    }
+
+    fn get_id(&self) -> &T {
+        &self.id
+    }
+}
+
+#[cfg(test)]
+mod restaurant_id_tests {
+    use super::*;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_restaurant_id() {
+        let id = Uuid::now_v7();
+        let restaurant_id = RestaurantId::new(id);
+        assert_eq!(restaurant_id.get_id(), &id);
+    }
+}
