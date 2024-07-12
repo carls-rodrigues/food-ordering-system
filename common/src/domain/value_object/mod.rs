26384ab10e10cc6c2c2af07fb4778a5b52b1185f
Carlos Rodrigues <cerf@furg.br>
2024-07-12 20:27:58 -0300

    feat: Add missing value objects for domain entities

--- common/src/domain/value_object/mod.rs
+++ common/src/domain/value_object/mod.rs
@@ -1,3 +1,15 @@
 mod base_id;
+mod customer_id;
 mod money;
 mod order_id;
+mod order_status;
+mod product_id;
+mod restaurant_id;
+
+pub use base_id::BaseId;
+pub use customer_id::CustomerId;
+pub use money::Money;
+pub use order_id::OrderId;
+pub use order_status::OrderStatus;
+pub use product_id::ProductId;
+pub use restaurant_id::RestaurantId;
