25208a2412528fb39aaf9b9e5618745f1948a4ea
Carlos Rodrigues <cerf@furg.br>
2024-07-12 20:17:39 -0300

    refactor: Remove commented out code in BaseEntity trait

--- common/src/domain/entity/base_entity.rs
+++ common/src/domain/entity/base_entity.rs
@@ -1,19 +1,5 @@
-// #[derive(Debug, Clone, PartialEq)]
 pub trait BaseEntity<ID: PartialEq> {
     fn new(id: ID) -> Self;
     fn get_id(&self) -> &ID;
     fn set_id(&mut self, id: ID);
-    // pub id: ID,
 }
-
-// impl<ID> BaseEntity<ID> {
-//     pub fn new(id: ID) -> Self {
-//         Self { id }
-//     }
-//     pub fn get_id(&self) -> &ID {
-//         &self.id
-//     }
-//     pub fn set_d(&mut self, id: ID) {
-//         self.id = id;
-//     }
-// }
