bb4fcc7c4819b329edec17c205228f49824fca3b
Carlos Rodrigues <cerf@furg.br>
2024-07-12 23:00:36 -0300

    feat: Add TrackingId value object for domain entities

---
+++ order-service/order-domain/domain-core/src/domain/value_object/tracking_id.rs
@@ -0,0 +1,34 @@
+use common::domain::value_object::BaseId;
+
+#[derive(Debug, PartialEq)]
+pub struct TrackingId<T>
+where
+    T: From<uuid::Uuid> + Into<uuid::Uuid>,
+{
+    id: T,
+}
+impl<T> BaseId<T> for TrackingId<T>
+where
+    T: From<uuid::Uuid> + Into<uuid::Uuid>,
+{
+    fn new(id: T) -> Self {
+        Self { id }
+    }
+
+    fn get_id(&self) -> &T {
+        &self.id
+    }
+}
+
+#[cfg(test)]
+mod tracking_id_tests {
+    use super::*;
+    use uuid::Uuid;
+
+    #[test]
+    fn test_tracking_id() {
+        let id = Uuid::now_v7();
+        let tracking_id = TrackingId::new(id);
+        assert_eq!(tracking_id.get_id(), &id);
+    }
+}
