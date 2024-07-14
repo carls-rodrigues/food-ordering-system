da137f3f7ed7f30c58c9034fd5d6d546d358ce43
Carlos Rodrigues <cerf@furg.br>
2024-07-13 21:38:30 -0300

    refactor: Rename get_id method in TrackingId to get_value

--- order-service/order-domain/domain-core/src/domain/value_object/tracking_id.rs
+++ order-service/order-domain/domain-core/src/domain/value_object/tracking_id.rs
@@ -1,6 +1,6 @@
 use common::domain::value_object::BaseId;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct TrackingId<T>
 where
     T: From<uuid::Uuid> + Into<uuid::Uuid>,
@@ -15,7 +15,7 @@ where
         Self { id }
     }
 
-    fn get_id(&self) -> &T {
+    fn get_value(&self) -> &T {
         &self.id
     }
 }
@@ -29,6 +29,6 @@ mod tracking_id_tests {
     fn test_tracking_id() {
         let id = Uuid::now_v7();
         let tracking_id = TrackingId::new(id);
-        assert_eq!(tracking_id.get_id(), &id);
+        assert_eq!(tracking_id.get_value(), &id);
     }
 }
