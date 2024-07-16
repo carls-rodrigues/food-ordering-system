0cb815d9bbd509cf643f6087117966daea7b9f18
Carlos Rodrigues <cerf@furg.br>
2024-07-16 11:24:29 -0300

    feat: Add order repository trait and implementation

---
+++ order-service/order-domain/domain-application-service/src/domain/ports/output/repository/order_repository.rs
@@ -0,0 +1,12 @@
+use async_trait::async_trait;
+use order_domain_core::domain::{entity::Order, value_object::TrackingId};
+use uuid::Uuid;
+
+#[async_trait]
+pub trait OrderRepository {
+    async fn save(&self, order: Order) -> Result<Order, String>;
+    async fn find_by_tracking_id(
+        &self,
+        tracking_id: TrackingId<Uuid>,
+    ) -> Result<Option<Order>, String>;
+}
