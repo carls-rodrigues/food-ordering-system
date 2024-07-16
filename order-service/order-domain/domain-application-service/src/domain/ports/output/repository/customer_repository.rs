77a1ad3a101ed5853a1dd54cd43496f4c64b1251
Carlos Rodrigues <cerf@furg.br>
2024-07-16 11:24:24 -0300

    feat: Add customer repository trait and implementation

---
+++ order-service/order-domain/domain-application-service/src/domain/ports/output/repository/customer_repository.rs
@@ -0,0 +1,8 @@
+use async_trait::async_trait;
+use order_domain_core::domain::entity::Customer;
+use uuid::Uuid;
+
+#[async_trait]
+pub trait CustomerRepository {
+    async fn find_customer(&self, customer_id: Uuid) -> Result<Option<Customer>, String>;
+}
