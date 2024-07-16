82af974b608dcd1ba42839c22ed409019520e18f
Carlos Rodrigues <cerf@furg.br>
2024-07-16 11:23:58 -0300

    feat: Add order cancelled payment request message publisher module and trait

---
+++ order-service/order-domain/domain-application-service/src/domain/ports/output/repository/message/publisher/payment/order_cancelled_payment_request_message_publisher.rs
@@ -0,0 +1,8 @@
+use common::domain::events::publisher::DomainEventPublisher;
+use order_domain_core::domain::event::OrderCancelledEvent;
+
+
+pub trait OrderCancelledPaymentRequestMessagePublisher:
+    DomainEventPublisher<OrderCancelledEvent>
+{
+}
